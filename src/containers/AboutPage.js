576091f9194d707083bf2ff8094634de4a9ca284
Haris Sucipto <harisssucipto@gmail.com>
2019-05-03 21:03:28 +0700

    tambah halaman about

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -2,10 +2,8 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import { Link } from 'react-router-dom';
 
-import latih from '../img/latih.png';
+import putra from '../img/putra.jpg';
 
 export default class PetunjukPage extends Component {
   render() {
@@ -13,20 +11,16 @@ export default class PetunjukPage extends Component {
       <Container>
         <Row className="mt-5 text-center">
           <Col>
-            <h2>Tentang Aplikasi SISKAWIT</h2>
-          </Col>
-        </Row>
-        <Row className="mt-5">
-          <Col md="6">
-            <img src={latih} alt="putra" />
-          </Col>
-          <Col md="6" className="mt-lg-5">
-            Aplikasi SISKAWIT merupakan aplikasi peramalan kepala sawit di
-            perkebunan ptpnV, yang dikembangkan oleh <b>Sahputra</b> NIM
-            (150000), jurusan ilmu komputer FMIPA Universitass Riau yang
-            digunakan untuk melakukan peramalan produksi sawit dengan menggunakn
-            metode multiple regresi linear sebagai syarat untuk menyelesaikan
-            perkuliahan sarjana starta 1.s
+            <h2 className="mb-5">Tentang Aplikasi SISKAWIT</h2>
+            <img src={putra} alt="putra" height="50%" />
+            <div className="mt-5">
+              Aplikasi SISKAWIT merupakan aplikasi peramalan kepala sawit pada
+              perkebunan di, yang dikembangkan oleh <b>Sahputra</b> NIM
+              (150000), jurusan ilmu komputer FMIPA Universitass Riau yang
+              digunakan untuk melakukan peramalan produksi sawit dengan
+              menggunakn metode multiple regresi linear sebagai syarat untuk
+              menyelesaikan perkuliahan sarjana starta 1.
+            </div>
           </Col>
         </Row>
       </Container>
