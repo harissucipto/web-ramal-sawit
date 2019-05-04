86a4c3b4870868eb76003ffc36ccff2cf68f4713
Haris Sucipto <harisssucipto@gmail.com>
2019-05-04 14:43:39 +0700

    hialngkan saat tidak ada

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import { Subscribe } from 'unstated';
+
+import DataPengguna from '../containers/DataPengguna';
 
 export default class Footer extends Component {
   render() {
     return (
-      <Navbar bg="dark" className="justify-content-center" sticky="bottom">
-        <p className="text-white">Copyright - SAHPURTRA - 2019</p>
-      </Navbar>
+      <Subscribe to={[DataPengguna]}>
+        {data =>
+          data.state.uid ? (
+            <Navbar
+              bg="dark"
+              className="justify-content-center"
+              sticky="bottom"
+            >
+              <p className="text-white">Copyright - SAHPURTRA - 2019</p>
+            </Navbar>
+          ) : null
+        }
+      </Subscribe>
     );
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,83 +3,93 @@ import { withRouter } from 'react-router-dom';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Subscribe } from 'unstated';
 
 import * as ROUTES from '../constants/routes';
+import DataPengguna from '../containers/DataPengguna';
 import LogoutButton from './LogoutButton';
 import palm from '../img/icon-palm.png';
 
 const Header = ({ location, history, logout }) => {
   const pathname = location.pathname;
   return (
-    <Navbar sticky="top" expand="md" bg="dark" variant="dark">
-      <Navbar.Brand onClick={() => history.push(ROUTES.HOME)}>
-        <img
-          src={palm}
-          width="30"
-          height="30"
-          className="d-inline-block align-top mr-3"
-          alt="React Bootstrap logo"
-        />
-        SISKAWIT
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse className="justify-content-end">
-        <Nav className="mr-2">
-          <Nav.Link
-            active={pathname === ROUTES.HOME}
-            onClick={() => history.push(ROUTES.HOME)}
-          >
-            Home
-          </Nav.Link>
-          <Nav.Link
-            active={pathname === ROUTES.LATIH}
-            onClick={() => history.push(ROUTES.LATIH)}
-          >
-            Latih Model
-          </Nav.Link>
-          <Nav.Link
-            active={pathname === ROUTES.ANALISA}
-            onClick={() => history.push(ROUTES.ANALISA)}
-          >
-            Analisa Model
-          </Nav.Link>
-          <Nav.Link
-            active={pathname === ROUTES.PERAMALAN}
-            onClick={() => history.push(ROUTES.PERAMALAN)}
-          >
-            Peramalan Produksi
-          </Nav.Link>
+    <Subscribe to={[DataPengguna]}>
+      {data =>
+        data.state.uid ? (
+          <Navbar sticky="top" expand="md" bg="dark" variant="dark">
+            <Navbar.Brand onClick={() => history.push(ROUTES.HOME)}>
+              <img
+                src={palm}
+                width="30"
+                height="30"
+                className="d-inline-block align-top mr-3"
+                alt="React Bootstrap logo"
+              />
+              SISKAWIT
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className="justify-content-end">
+              <Nav className="mr-2">
+                <Nav.Link
+                  active={pathname === ROUTES.HOME}
+                  onClick={() => history.push(ROUTES.HOME)}
+                >
+                  Home
+                </Nav.Link>
+                <Nav.Link
+                  active={pathname === ROUTES.LATIH}
+                  onClick={() => history.push(ROUTES.LATIH)}
+                >
+                  Latih Model
+                </Nav.Link>
+                <Nav.Link
+                  active={pathname === ROUTES.ANALISA}
+                  onClick={() => history.push(ROUTES.ANALISA)}
+                >
+                  Analisa Model
+                </Nav.Link>
+                <Nav.Link
+                  active={pathname === ROUTES.PERAMALAN}
+                  onClick={() => history.push(ROUTES.PERAMALAN)}
+                >
+                  Peramalan Produksi
+                </Nav.Link>
 
-          <NavDropdown
-            title="Akun"
-            id="collasible-nav-dropdown"
-            active={pathname === ROUTES.AKUN || pathname === ROUTES.SETTING}
-          >
-            <NavDropdown.Item
-              active={pathname === ROUTES.AKUN}
-              onClick={() => history.push(ROUTES.AKUN)}
-            >
-              Informasi
-            </NavDropdown.Item>
-            <NavDropdown.Item
-              active={pathname === ROUTES.SETTING}
-              onClick={() => history.push(ROUTES.SETTING)}
-            >
-              {' '}
-              Setting
-            </NavDropdown.Item>
-            <hr />
-            <LogoutButton logout={logout} />
-          </NavDropdown>
-          <Nav.Link
-            active={pathname === ROUTES.ABOUT}
-            onClick={() => history.push(ROUTES.ABOUT)}
-          >
-            About
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+                <NavDropdown
+                  title="Akun"
+                  id="collasible-nav-dropdown"
+                  active={
+                    pathname === ROUTES.AKUN || pathname === ROUTES.SETTING
+                  }
+                >
+                  <NavDropdown.Item
+                    active={pathname === ROUTES.AKUN}
+                    onClick={() => history.push(ROUTES.AKUN)}
+                  >
+                    Informasi
+                  </NavDropdown.Item>
+                  <NavDropdown.Item
+                    active={pathname === ROUTES.SETTING}
+                    onClick={() => history.push(ROUTES.SETTING)}
+                  >
+                    {' '}
+                    Setting
+                  </NavDropdown.Item>
+                  <hr />
+                  <LogoutButton logout={logout} />
+                </NavDropdown>
+                <Nav.Link
+                  active={pathname === ROUTES.ABOUT}
+                  onClick={() => history.push(ROUTES.ABOUT)}
+                >
+                  About
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        ) : null
+      }
+    </Subscribe>
   );
 };
 
